3126474031698ae1a69e26afeceffc837f4c654c
Sequenta <tranquilized.mind@gmail.com>
2013-07-11 15:32:00 +0600

    Шаблон "Одиночка"

---
+++ Patterns/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            //var uniqueShirt = new UniqueShirt(); Cannot access private constructor
+            var uniqueShirt = UniqueShirt.GetInstance();
+            Console.WriteLine("Authograph on unique t-shirt:{0}",uniqueShirt.Autograph);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Patterns/Singleton/UniqueShirt.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton
+{
+    public class UniqueShirt
+    {
+        private static UniqueShirt instance;
+        public string Autograph { get; private set; }
+
+        private UniqueShirt()
+        {
+            Autograph = "Very famous football player!";
+            instance = this;
+        }
+
+        public static UniqueShirt GetInstance()
+        {
+            return instance ?? new UniqueShirt();
+        }
+    }
+}
